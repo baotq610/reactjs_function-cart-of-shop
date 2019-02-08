3efe7249e225aac4594a7420ba255e60a83a6264
baotq610 <baotq610@gmail.com>
2019-02-08 17:56:30 +0700

    Reactjs - Chức năng giỏ hàng

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Header from './components/Header';
+import ProductsContainer from './containers/ProductsContainer';
+import MessageContainer from './containers/MessageContainer';
+import CartContainer from './containers/CartContainer';
+import Footer from './components/Footer';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                {/* <!-- Header --> */}
+                <Header />
+                <main id="mainContainer">
+                    <div className="container">
+                        {/* <!-- Products --> */}
+                        <ProductsContainer />
+                        {/* <!-- Message --> */}
+                        <MessageContainer />
+                        {/* <!-- Cart --> */}
+                        <CartContainer />
+                    </div>
+                </main>
+                {/* <!-- Footer --> */}
+                <Footer />
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/CartItem.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import * as Message from './../constants/Message'
+
+
+class CartItem extends Component {
+
+  render() {
+      var {item} = this.props;      
+      var { quantity }    = item;
+      
+    return (
+        <tr>
+                        <th scope="row">
+                            <img src={item.product.image}
+                                alt="" className="img-fluid z-depth-0" />
+                        </th>
+                        <td>
+                            <h5>
+                                <strong>{item.product.name}</strong>
+                            </h5>
+                        </td>
+                        <td>{item.product.price}$</td>
+                        <td className="center-on-small-only">
+                            <span className="qty">{quantity} </span>
+                            <div className="btn-group radio-group" data-toggle="buttons">
+                                <label className="btn btn-sm btn-primary
+                                    btn-rounded waves-effect waves-light" onClick={ () => this.onUpdateQuantity(item.product, item.quantity - 1)}>
+                                    <a>—</a>
+                                </label>
+                                <label className="btn btn-sm btn-primary
+                                    btn-rounded waves-effect waves-light" onClick={ () => this.onUpdateQuantity(item.product, item.quantity + 1)}>
+                                    <a>+</a>
+                                </label>
+                            </div>
+                        </td>
+                        <td>{this.showSubTotal(item.product.price,item.quantity)}$</td>
+                        <td>
+                            <button type="button" className="btn btn-sm btn-primary waves-effect waves-light" data-toggle="tooltip" data-placement="top"
+                                title="" data-original-title="Remove item" onClick={()=>this.onDeleteProductInCart(item.product)}>
+                                X
+                            </button>
+                        </td>
+                    </tr>
+    );
+  }
+  showSubTotal = (price, quantity) => {
+        return price * quantity;
+  }
+  onDeleteProductInCart = (product) => {
+        var { onDeleteProductInCart,onChangeMessage } = this.props;
+        onDeleteProductInCart(product)
+        onChangeMessage(Message.MSG_DELETE_TO_PRODUCT_IN_CART_SUCCESS)
+  }
+  onUpdateQuantity = (product, quantity) => {
+      var { onChangeMessage } = this.props;
+        if (quantity > 0) {
+            this.setState({
+                quantity : quantity
+            })
+            this.props.onUpdateProductInCart(product, quantity)
+        }
+        onChangeMessage(Message.MSG_UPDATE_TO_PRODUCT_IN_CART_SUCCESS)
+  }
+}
+
+export default CartItem;
